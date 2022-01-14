2b470692fc065bf09726e8a63b1ce2537766ac06
kevin1688 <jackexi@gmail.com>
2022-01-14 10:52:51 +0800

    Initial Commit

---
+++ iBMR/iBMRApp.swift
@@ -0,0 +1,17 @@
+//
+//  iBMRApp.swift
+//  iBMR
+//
+//  Created by kai wen chen on 2022/1/14.
+//
+
+import SwiftUI
+
+@main
+struct iBMRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
